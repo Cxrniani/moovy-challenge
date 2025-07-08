ddbe35f4c59de9d0a90169b09ea0d4cbe905e935
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-08 13:26:33 -0300

    feat: add MovieCard
    + material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,47 @@
-import MovieCard from "./components/MovieCard"
+import './index.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+
+  const movies = [
+    {
+      id: 1,
+      title: 'Duna: Parte Dois',
+      imageUrl: 'https://image.tmdb.org/t/p/w500/A0K03oB41wOq6t8rO4D620d4fR3.jpg', // Exemplo de imagem
+      rating: 8.5,
+    },
+    {
+      id: 2,
+      title: 'Oppenheimer',
+      imageUrl: 'https://image.tmdb.org/t/p/w500/fm6KqXz3M2M1k3Cdz5wR9eJz1mQ.jpg',
+      rating: 8.3,
+    },
+    {
+      id: 3,
+      title: 'Interestelar',
+      imageUrl: 'https://image.tmdb.org/t/p/w500/uxy2VfT0qY14d1XJg4c0Y1mY8jU.jpg',
+      rating: 8.6,
+    },{
+      id: 4,
+      title: 'A Volta dos que Não Foram',
+      imageUrl: 'https://image.tmdb.org/t/p/w500/uxy2VfT0qY14d1XJg4c0Y1mY8jU.jpg',
+      rating: 100.0,
+    }
+  ];
 
   return (
     <div className="px-60 pt-10">
       <div className="flex-col h-screen">
         <h1 className="text-2xl text-stone-800">My Library</h1>
-        <div className="flex-row flex-wrap gap-10 mt-10">
-        
+        <div className="flex flex-row mt-10">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              imageUrl={movie.imageUrl}
+              title={movie.title}
+              rating={movie.rating}
+            />
+          ))}
         </div>
       </div>
     </div>
--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,8 +1,63 @@
+import { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function MovieCard() {
-  return (
-    <div>MovieCard</div>
-  )
+interface MovieCardProps {
+  imageUrl: string;
+  title: string;
+  rating: number;
 }
+
+function MovieCard({ imageUrl, title, rating }: MovieCardProps) {
+  const [isInMyList, setIsInMyList] = useState(false);
+
+  const handleToggleMyList = () => {
+    setIsInMyList((prev) => !prev);
+  };
+return (
+    <Card className="w-72 h-fit py-2 px-1 mx-auto my-4 shadow-xl shadow-black rounded-xl">
+      <CardMedia
+        component="img"
+        className="w-full h-80 object-cover"
+        image={imageUrl}
+        alt={title}
+      />
+
+      <CardContent className="p-4">
+        <div className="flex justify-between over items-center mb-2">
+          <Typography
+            gutterBottom
+            variant="h6" 
+            component="div"
+            className="text-lg h-6 font-semibold items-start text-stone-800 " 
+          >
+            {title}
+          </Typography>
+
+          <div className="flex items-center text-gray-800 text-md font-bold px-3 rounded-full">
+            <span className="mr-1">⭐</span>
+            {rating.toFixed(1)}
+          </div>
+        </div>
+      </CardContent>
+
+      <CardActions className="p-4 mt-2">
+        <Button
+          size="small"
+          variant="contained"
+          color={isInMyList ? "error" : "primary"} 
+          onClick={handleToggleMyList}
+          className="w-full">
+          {isInMyList ? 'Remover da Lista' : 'Adicionar à Lista'}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
 
 export default MovieCard
