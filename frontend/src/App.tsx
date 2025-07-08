1979ce20c0f2b9824749a3218832f87398000a4a
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-08 12:49:59 -0300

    feat: add home/library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import MovieCard from "./components/MovieCard"
 
 function App() {
 
   return (
-    <div>Page</div>
+    <div className="px-60 pt-10">
+      <div className="flex-col h-screen">
+        <h1 className="text-2xl text-stone-800">My Library</h1>
+        <div className="flex-row flex-wrap gap-10 mt-10">
+        
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ function NavBar() {
       <a href="/">
         <div className="text-orange-400 p-5 text-4xl font-black">Moovy</div>
       </a>
-      <div className="max-container w-full flex px-20">
+      <div className="max-container w-full flex px-24">
         <nav className="flex w-full gap-x-16">
           <a href="/movies" className="text-stone-900 text-l font-black hover:text-gray-400">Search</a>
           <a href="/about" className="text-orange-400 text-l font-black hover:text-gray-400">My Library</a>
