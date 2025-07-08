1d3fa59cb99ccd83488167f51a31026f7ac48269
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-08 15:22:38 -0300

    refactor: add routes para alternar entre lib e search

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import NavBar from './components/NavBar.tsx'
+import Library from './components/Library.tsx';
+import Search from './components/SearchMovie.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <div>
   <StrictMode>
-    <NavBar />
-    <App />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<App />} /> 
+        <Route path="/movies" element={<Search movies={movies} />} /> 
+        <Route path="/my-library" element={<Library movies={movies} />} /> 
+      </Routes>
+    </Router>
   </StrictMode>
-  </div>
 )
