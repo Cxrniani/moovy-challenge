06949a0f01521e1520d7c8959c35cba79c7515cd
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-08 15:47:22 -0300

    refactor: navbar agora usa navlinks do react-router

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,33 @@
+import { NavLink } from 'react-router-dom';
+
 function NavBar() {
   return (
-    <div className="flex items-center p-4">
-      <a href="/">
+    <div className="flex items-center p-4 ">
+      <NavLink to="/">
         <div className="text-orange-400 p-5 text-4xl font-black">Moovy</div>
-      </a>
+      </NavLink>
       <div className="max-container w-full flex px-24">
         <nav className="flex w-full gap-x-16">
-          <a href="/movies" className="text-stone-900 text-l font-black hover:text-gray-400">Search</a>
-          <a href="/about" className="text-orange-400 text-l font-black hover:text-gray-400">My Library</a>
+          <NavLink
+            to="/movies"
+            className={({ isActive }) =>
+              isActive
+                ? "text-orange-400 text-l font-black hover:text-orange-500 ease-in-out duration-100"
+                : "text-stone-800 text-l font-black hover:text-orange-500 ease-in-out duration-100"
+            }
+          >
+            Search
+          </NavLink>
+          <NavLink
+            to="/my-library"
+            className={({ isActive }) =>
+              isActive
+                ? "text-orange-400 text-l font-black hover:text-orange-500 ease-in-out duration-100"
+                : "text-stone-800 text-l font-black hover:text-orange-500 ease-in-out duration-100"
+            }
+          >
+            My Library
+          </NavLink>
         </nav>
       </div>
     </div>
