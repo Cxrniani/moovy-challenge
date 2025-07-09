c3f9d0a197fe592e7c3574c4dd491c6a7df7802c
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-09 13:26:30 -0300

    feat: add funcoes do card na biblioteca

--- frontend/src/components/Library.tsx
+++ frontend/src/components/Library.tsx
@@ -1,27 +1,45 @@
-import React from 'react'
 import MovieCard from './MovieCard';
 
+interface Movie {
+  id: string;
+  title: string;
+  imageUrl: string;
+  rating: number;
+}
+
+interface LibraryProps {
+  movies: Movie[];
+  onRemoveMovie: (movieId: string) => Promise<void>;
+}
 
 function Library({ movies }: { movies: { id: string; title: string; imageUrl: string; rating: number }[] }) {
   return (
-        <div className="pt-10">
+    <div className="pt-10">
       <div className="px-40 flex-col h-screen">
         <h1 className="text-2xl text-stone-800">My Library</h1>
         <div className='flex justify-between items-center'>
           <div className="grid grid-cols-1 gap-5 lg:grid-cols-5 md:grid-cols-4 mt-6">
-            {movies.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                imageUrl={movie.imageUrl}
-                title={movie.title}
-                rating={movie.rating}
-              />
-            ))}
+            {movies.length > 0 ? (
+              movies.map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  id={movie.id}
+                  imageUrl={movie.imageUrl}
+                  title={movie.title}
+                  rating={movie.rating}
+                  onAdd={() => {}}
+                  onRemove={onRemoveMovie}
+                  isAdded={true}
+                />
+              ))
+            ) : (
+              <p className="text-stone-600 text-lg col-span-full flex justify-center items-center h-64 w-full">Sua biblioteca está vazia. Adicione filmes pela página de busca!</p>
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Library
