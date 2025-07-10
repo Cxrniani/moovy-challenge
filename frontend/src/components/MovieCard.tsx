b33ec69108f502c2c707afcf96eae64fbdd5b4ea
Matheus Corniani <matheuscorniani@gmail.com>
2025-07-10 14:39:01 -0300

    style: add confirmação de exclusao

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -5,6 +5,11 @@ import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 interface MovieCardProps {
   id: string;
@@ -18,58 +23,109 @@ interface MovieCardProps {
 
 function MovieCard({ id, imageUrl, title, rating, onAdd, onRemove, isAdded }: MovieCardProps) {
   const [isInMyList, setIsInMyList] = useState(isAdded);
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
 
   useEffect(() => {
     setIsInMyList(isAdded);
   }, [isAdded]);
 
-  const handleToggleMyList = () => {
-    if (isInMyList) {
-      onRemove?.(id);
-    } else {
-      onAdd?.({ id, title, imageUrl, rating });
-    }
+  const handleRemoveClick = () => {
+    setOpenConfirmDialog(true);
   };
-return (
-    <Card className="w-72 h-fit py-2 px-1 mx-auto my-4 shadow-xl shadow-black rounded-xl">
-      <CardMedia
-        component="img"
-        className="w-full h-80 object-cover"
-        image={imageUrl}
-        alt={title}
-      />
 
-      <CardContent className="p-4">
-        <div className="flex justify-between over items-center mb-2">
-          <Typography
-            gutterBottom
-            variant="h6" 
-            component="div"
-            className="text-lg h-6 font-semibold items-start text-stone-800 " 
-          >
-            {title}
-          </Typography>
+  const handleConfirmRemove = () => {
+    onRemove?.(id);
+    setOpenConfirmDialog(false);
+  };
 
-          <div className="flex items-center text-gray-800 text-md font-bold px-3 rounded-full">
-            <span className="mr-1">⭐</span>
-            {rating.toFixed(1)}
+  const handleCloseDialog = () => {
+    setOpenConfirmDialog(false);
+  };
+
+  const handleAddClick = () => {
+    onAdd?.({ id, title, imageUrl, rating });
+  };
+
+  return (
+    <>
+      <Card className="h-fit py-2 px-1 mx-auto my-4 shadow-xl shadow-black rounded-xl"
+      sx={{width: '18rem'}}>
+        <CardMedia
+          component="img"
+          className="w-full h-80 object-cover"
+          image={imageUrl}
+          alt={title}
+        />
+        <CardContent className="p-4">
+          <div className="flex justify-between over items-center mb-2">
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="div"
+              className="text-lg h-6 font-semibold items-start text-stone-800 "
+            >
+              {title}
+            </Typography>
+            <div className="flex items-center text-gray-800 text-md font-bold px-3 rounded-full">
+              <span className="mr-1">⭐</span>
+              {rating.toFixed(1)}
+            </div>
           </div>
-        </div>
-      </CardContent>
+        </CardContent>
+        <CardActions className="p-4 mt-2">
+          <Button
+            size="small"
+            variant="contained"
+            color={isInMyList ? 'error' : 'primary'}
+            onClick={isInMyList ? handleRemoveClick : handleAddClick}
+            className="w-full"
+          >
+            {isInMyList ? 'Remover da Lista' : 'Adicionar à Lista'}
+          </Button>
+        </CardActions>
+      </Card>
 
-      <CardActions className="p-4 mt-2">
-        <Button
-          size="small"
-          variant="contained"
-          color={isInMyList ? "error" : "primary"} 
-          onClick={handleToggleMyList}
-          className="w-full">
-          {isInMyList ? 'Remover da Lista' : 'Adicionar à Lista'}
-        </Button>
-      </CardActions>
-    </Card>
+      <Dialog
+        open={openConfirmDialog}
+        onClose={handleCloseDialog}
+        PaperProps={{
+          sx: {
+            borderRadius: '16px', // Bordas arredondadas
+            width: '1000px',
+          },
+        }}
+      >
+        <DialogTitle sx={{ fontWeight: 'bold' }}>
+          Remove from your library
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText
+          sx={{ fontSize: '20px', color: '#333', padding: '30px 10px' }}>
+            Are you sure you want to remove "{title}" from your library? It contains an audio review and you will lose it if you remove.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ padding: '16px 24px' }}>
+          <Button 
+            onClick={handleCloseDialog} 
+            variant='contained'
+            color='info'
+            sx={{ borderRadius: '8px', width: '50%' }}
+          >
+            Cancel
+          </Button>
+          <Button 
+            onClick={handleConfirmRemove} 
+            variant='contained'
+            color = "error"
+            sx={{ borderRadius: '8px', width: '50%' }} 
+            autoFocus
+          >
+            Remove
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
-
-export default MovieCard
+export default MovieCard;
